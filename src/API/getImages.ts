cdad3c57dd8462862154fe9e47bee9ceb0227b85
alinidi <alinareyz@gmail.com>
2025-07-12 19:42:45 +0500

    feat: add a fallback URL in case getConfigEndpoin fails

--- src/API/getImages.ts
+++ src/API/getImages.ts
@@ -2,7 +2,8 @@ import { getConfigEndpoint } from './getConfigEndpoint';
 
 export async function getImages(ids: number[]) {
   try {
-    const configUrl = await getConfigEndpoint();
+    const configUrl =
+      (await getConfigEndpoint()) || 'https://www.artic.edu/iiif/2';
     const urls = ids.map((id) => {
       if (id === null) {
         return 'https://img.freepik.com/premium-vector/shades-gray-scale-color-palette-vector-illustration-eps-10_213497-3330.jpg?semt=ais_hybrid&w=740';
