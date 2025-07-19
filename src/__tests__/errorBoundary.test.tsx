2020c296ee5a59dda1e68114f94eafa84a2210a2
alinidi <alinareyz@gmail.com>
2025-07-19 12:17:44 +0500

    feat: add test for ErrorBoundary

---
+++ src/__tests__/errorBoundary.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { it, vi } from 'vitest';
+import { ErrorBoundary } from '../components/ErrorBoundary/ErrorBoundary';
+import '@testing-library/jest-dom';
+
+describe('errorBoundary', () => {
+  it('Catches and handles JavaScript errors in child components', async () => {
+    const ProblemComponent = () => {
+      throw new Error('Crashed');
+    };
+
+    render(
+      <ErrorBoundary>
+        <ProblemComponent />
+      </ErrorBoundary>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText('Something went wrong'));
+    });
+  });
+
+  it('Logs error to console', async () => {
+    const spyLog = vi.spyOn(console, 'log');
+
+    const ProblemComponent = () => {
+      throw new Error('Crashed');
+    };
+
+    render(
+      <ErrorBoundary>
+        <ProblemComponent />
+      </ErrorBoundary>
+    );
+
+    await waitFor(() => {
+      expect(spyLog).toHaveBeenCalled();
+      const [error] = spyLog.mock.calls[0];
+      expect(error).toBeInstanceOf(Error);
+      expect(error.message).toBe('Crashed');
+    });
+  });
+});
