61cc99e34655a6c53a85e8fbb18c7de533f0e865
alinidi <alinareyz@gmail.com>
2025-08-24 20:10:44 +0500

    fix: fix vite.config

--- rs-react-app/vite.config.ts
+++ rs-react-app/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
 export default defineConfig({
+  base: './',
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/main.{js,jsx,ts,tsx}',
+        'src/app.{js,jsx,ts,tsx}',
+        'src/**/*.d.ts',
+      ],
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+});
